102deac628b06689b75fffcdaa1a81363c05db06
Jack <donahuej5@wit.edu>
2025-10-06 09:04:25 -0400

    userdata imports work

--- main.py
+++ main.py
@@ -14,10 +14,30 @@ loggedin = False
 
 # Functions
 # Updates the users data in both the program var & the CSV
-def updateUser():
+def readUser(userid):
     global userdata
-    pass
-    # Read from & sets users totalincome & totaldebt and calculates networth
+    totaldebt = 0
+    totalincome = 0
+    with open("data/"+userid+"/debt.csv", mode="r") as data:
+        csv_reader = csv.reader(data)
+        iteration = 0
+        for row in csv_reader:
+            if iteration != 0:
+                totaldebt += float(row[3])
+                print(row)
+            iteration += 1
+        userdata[5] = totaldebt
+    with open("data/"+userid+"/income.csv", mode="r") as data:
+        csv_reader = csv.reader(data)
+        iteration = 0
+        for row in csv_reader:
+            if iteration != 0:
+                totalincome += float(row[1])
+                print(row)
+            iteration += 1
+        userdata[6] = totalincome
+        userdata[4] = totalincome - totaldebt
+            
 
 
 # Program start
@@ -25,16 +45,16 @@ while not loggedin and not userQuit:
     userdata[0] = input("\nWelcome, please enter your user ID: ")
     userdata[1] = input("Now, please enter your password: ")
     # Imports data from userdata.csv to the userdata list, then logs the signin
-
     with open("data/userlist.csv", mode="r") as data:
         csv_reader = csv.reader(data)
         for row in csv_reader:
             if row[0] == userdata[0] and row[1] == userdata[1]:
-                userdata[1] = row[2] # Firstname
-                userdata[2] = row[3] # Lastname
-                userdata[3] = row[4] # Networth
-                userdata[4] = row[5] # Total Debt
-                userdata[5] = row[6] # Total Income
+                readUser(userdata[0])
+                userdata[2] = row[2] # Firstname
+                userdata[3] = row[3] # Lastname
+                #userdata[4] = row[4] # Networth
+                #userdata[5] = # Total Debt
+                #userdata[6] = row[6] # Total Income
                 loggedin = True
                 log_entry = {
                     "userid": userdata[0],
@@ -55,7 +75,7 @@ while not loggedin and not userQuit:
             
         # When there is a sucessful login this block of code initiates the 'user session' loop
         if loggedin:
-            print ("[DEBUG] userdata: " + userdata[1], userdata[2], userdata[3])
+            print (userdata)
             print ("\nWelcome " + userdata[1] + " " + userdata[2] + "!")
 
             # While the user is still loggedin loop operations untill they logout
@@ -73,24 +93,6 @@ while not loggedin and not userQuit:
                     print("\nInvalid, try again\n")
         else:                
             print ("\nInvalid, try again")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
